19acf6e9b7dcae6cca28fe26cc9e068c5aa8830a
sri krishna Jarugubilli <55265709+srikrishnajarugubilli@users.noreply.github.com>
2019-12-11 19:55:09 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,27 @@
+import java.util.*;//importing required packages you can even code as import java.util.scanner; 
+class prime
+{
+	 public static void main(String[] args) {
+
+	 	int n,i,count=0;//Declaring required variables
+	 	Scanner s =new Scanner(System.in);//creating object for user input
+	 	n=s.nextInt();//initialization of user input to declared variable
+        //loop that checks given number is prime or not
+	 	for (i=1;i<=n;i++) {
+
+	 		if (n%i==0) {
+	 			count++;
+	 		}
+	 		
+	 	}//loop ends
+	 	if (count>2) {
+	 		System.out.println("The given number is not prime" +n);
+	 		
+	 	}
+	 	else
+	 	{
+	 	   System.out.println("The given number is  prime " +n);	
+	 	}
+		
+	}
+}
